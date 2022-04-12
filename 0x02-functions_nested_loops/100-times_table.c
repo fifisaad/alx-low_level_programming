9deac38ce6db1ebd07a0bfbcdbe957a4aab0d50c
Fatima Farah <fifisaad_99@hotmail.com>
2022-04-12 10:24:57 -0700

    time_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,14 +8,13 @@
 
 void times_table(void)
 {
-
 	int num, mult, prod;
 
 	if (n >= 0 && n <= 15)
-
 	{
 		for (num = 0; num <= n; num++)
 		{
+
 			_putchar('0');
 
 			for (mult = 1; mult <= n; mult++)
