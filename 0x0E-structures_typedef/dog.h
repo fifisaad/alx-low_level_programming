cb740fb5b9853d5352e7b527b3379412a7a26d8d
Fatima Farah <fifisaad_99@hotmail.com>
2022-05-10 16:19:39 -0700

    dog_mod

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,12 +17,15 @@ typedef struct dog
 	char *owner;
 }
 dog_t;
+
 #endif
 #ifndef _FUNCTIONS_H
 #ifndef _FUNCTIONS_H
+
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
