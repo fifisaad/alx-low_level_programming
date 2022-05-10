b58e18c1421c9c40afa38fd0b6bd446c36e017f6
fifisaad <68048261+fifisaad@users.noreply.github.com>
2022-05-11 01:58:00 +0200

    new_dog_update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,38 +12,38 @@ char *_strcpy(char *x, char *y);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	      dog_t *new_d;
+	dog_t *new_d;
 
-	      new_d = malloc(sizeof(dog_t));
-	      if (new_d == NULL)
-		            return (NULL);
-	      if (name == NULL)
-		            new_d->name = NULL;
-	      else
-	      {
-		            new_d->name = malloc(_strlen(name) + 1);
-		            if (new_d->name == NULL)
-		            {
-			                  free(new_d);
-			                  return (NULL);
-		            }
-		            new_d->name = _strcpy(new_d->name, name);
-	      }
-	      if (owner == NULL)
-		            new_d->owner = NULL;
-	      else
-	      {
-		            new_d->owner = malloc(_strlen(owner) + 1);
-		            if (new_d->owner == NULL)
-		            {
-                        free(new_d->name);
-                        free(new_d);
-                        return (NULL);
-		            }
-		            new_d->owner = _strcpy(new_d->owner, owner);
-	      }
-        new_d->age = age;
-        return (new_d);
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
+		return (NULL);
+	if (name == NULL)
+		new_d->name = NULL;
+	else
+	{
+		new_d->name = malloc(_strlen(name) + 1);
+		if (new_d->name == NULL)
+		{
+			free(new_d);
+			return (NULL);
+		}
+		new_d->name = _strcpy(new_d->name, name);
+	}
+	if (owner == NULL)
+		new_d->owner = NULL;
+	else
+	{
+		new_d->owner = malloc(_strlen(owner) + 1);
+		if (new_d->owner == NULL)
+		{
+			free(new_d->name);
+			free(new_d);
+			return (NULL);
+		}
+		new_d->owner = _strcpy(new_d->owner, owner);
+	}
+	new_d->age = age;
+	return (new_d);
 }
 /**
  * _strlen - gets len of str
@@ -53,11 +53,11 @@ dog_t *new_dog(char *name, float age, char *owner)
  */
 int _strlen(char *s)
 {
-	      int i = 0;
+	int i = 0;
 
-	      for (i = 0; s[i]; i++)
-		            ;
-	      return (i);
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
 }
 /**
  * _strcpy - copies string from y to x
@@ -69,10 +69,10 @@ int _strlen(char *s)
  */
 char *_strcpy(char *x, char *y)
 {
-	      char *a = x;
+	char *a = x;
 
-	      while (*y)
-		            *a++ = *y++;
-	      *a = '\0';
-	      return (x);
+	while (*y)
+		*a++ = *y++;
+	*a = '\0';
+	return (x);
 }
